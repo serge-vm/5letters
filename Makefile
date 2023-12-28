8a433d13d29ad7de30efe15c80f665aeba06b90d
serge-vm <47665719+serge-vm@users.noreply.github.com>
2023-12-28 13:50:46 +0300

    Feature basic api (#1)
    * solver api
    
    * folders structured

--- Makefile
+++ Makefile
@@ -1,4 +1,6 @@
-all: clean console api
+SWAG_VERSION := $(shell swag -v 2>/dev/null)
+
+all: clean docs console api
 
 .PHONY: console
 console:
@@ -9,6 +11,15 @@ console:
 api:
 	go build -o bin/5letters api/main.go
 	env GOOS=windows GOARCH=amd64 go build -o bin/5letters.exe api/main.go
+
+.PHONY: docs
+docs:	
+ifdef SWAG_VERSION
+	@echo "Found $(SWAG_VERSION)"
+	swag init -g ./api/main.go
+else
+	@echo "Error: swag not found. Documentation was not regenerated."
+endif
 
 clean:
 	rm -f bin/*
--- api/main.go
+++ api/main.go
@@ -1,7 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/serge-vm/5letters/api/handlers"
+	"github.com/serge-vm/5letters/docs"
+	swaggerfiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
+)
 
 func main() {
-	fmt.Println("Hello, API!")
+	r := gin.Default()
+	docs.SwaggerInfo.BasePath = "/api/v1"
+	v1 := r.Group("/api/v1")
+	{
+		v1.POST("/solver", handlers.SolverHandler)
+	}
+	r.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerfiles.Handler, ginSwagger.DefaultModelsExpandDepth(-1)))
+	r.Run(":8080")
+
 }
