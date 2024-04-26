b248397bcc77e8e21f81f14da15c467863d09a70
serge-vm <donkey.ia@gmail.com>
2024-04-26 14:59:13 +0000

    solver tests

--- pkg/quizsolver/main_test.go
+++ pkg/quizsolver/main_test.go
@@ -6,6 +6,64 @@ import (
 	"github.com/serge-vm/5letters/internal/models"
 )
 
+func TestSolverFindGrass(t *testing.T) {
+	var unorderedLetters []models.Unordered
+	unorderedLetters = append(unorderedLetters, models.Unordered{P: 1, L: "а"})
+	letters := make(map[int]string)
+	letters[4] = "в"
+	letters[1] = "т"
+	result := Solve(unorderedLetters, letters, []string{"ю"})
+	for _, r := range result {
+		if r == "трава" {
+			return
+		}
+	}
+	t.Fatal("Word трава not found, but it should be.")
+}
+
+func TestSolverFindNoGrass(t *testing.T) {
+	var unorderedLetters []models.Unordered
+	unorderedLetters = append(unorderedLetters, models.Unordered{P: 3, L: "а"})
+	letters := make(map[int]string)
+	letters[4] = "в"
+	letters[1] = "т"
+	result := Solve(unorderedLetters, letters, []string{"ю"})
+	for _, r := range result {
+		if r == "трава" {
+			t.Fatal("Word трава found, but it shouldn't be.")
+		}
+	}
+}
+
+func TestSolverFindMetro(t *testing.T) {
+	var unorderedLetters []models.Unordered
+	unorderedLetters = append(unorderedLetters, models.Unordered{P: 1, L: "о"})
+	letters := make(map[int]string)
+	letters[3] = "т"
+	letters[1] = "м"
+	result := Solve(unorderedLetters, letters, []string{"к"})
+	for _, r := range result {
+		if r == "метро" {
+			return
+		}
+	}
+	t.Fatal("Word метро not found, but it should be.")
+}
+
+func TestSolverFindNoMetro(t *testing.T) {
+	var unorderedLetters []models.Unordered
+	unorderedLetters = append(unorderedLetters, models.Unordered{P: 1, L: "о"})
+	letters := make(map[int]string)
+	letters[3] = "т"
+	letters[1] = "м"
+	result := Solve(unorderedLetters, letters, []string{"р"})
+	for _, r := range result {
+		if r == "метро" {
+			t.Fatal("Word метро found, but it shouldn't be.")
+		}
+	}
+}
+
 func TestUnorderedLetterFound(t *testing.T) {
 	var unorderedLetters []models.Unordered
 	unorderedLetters = append(unorderedLetters, models.Unordered{P: 1, L: "e"})
@@ -13,7 +71,6 @@ func TestUnorderedLetterFound(t *testing.T) {
 	if result == false {
 		t.Fatal("Letter e is present in word tests and not first. Misdetection.")
 	}
-
 }
 
 func TestUnorderedLetterExclude(t *testing.T) {
